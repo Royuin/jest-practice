a1c938f15af732ba0cdbd0218a068af3edec82be
Gage <77414756+Royuin@users.noreply.github.com>
2023-04-29 17:32:32 -0500

    made test for calculator addition

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from './script';
+import { capitalize, reverseString, calculator } from './script';
 
 test('capitalize first letter', () => {
   expect(capitalize('foo')).toBe('Foo');
@@ -6,4 +6,8 @@ test('capitalize first letter', () => {
 
 test('reverseString', () => {
   expect(reverseString('foo')).toBe('oof');
+});
+
+test('calculator addition', () => {
+  expect(calculator.add(3, 7)).toBe(10);
 });
